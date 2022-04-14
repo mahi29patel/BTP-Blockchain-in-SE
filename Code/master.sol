5f3ce3e4edfc4406c0bc040f2f784788c4abfb2d
Mahipatel29 <mahoo29patel@gmail.com>
2022-04-14 12:50:42 +0530

    penalty points added

--- Code/master.sol
+++ Code/master.sol
@@ -4,7 +4,6 @@ import 'task.sol';
 import 'task1.sol';
 import 'task2.sol';
 import 'task3.sol';
-
 contract master{
 
     address[4] addressT;
@@ -21,156 +20,143 @@ contract master{
     uint256[4] excp3= [60, 80, 70, 80];
 
     string[4] status;
-    int[4] penalty1;
+   
     uint256[4] time1;
     uint256[4] starttime1;
     uint256[4] diff;
-    address sender;
-    // mapping(address => uint) public penalty;
+    address send1;
+    mapping(address => uint) public penalty;
     task.price expp;
     task1.price expp1;
     task2.price expp2;
     task3.price expp3;
+    uint sprinttime;
+   
 
-    // function returnMappingValue(address _key) public view returns (uint) {
-    //     return penalty[_key];
-    // }  
-
-    // //compare function of time and price to calculate the penalty points
-    // function public compare(uint256 _time1, uint256 _starttime1, uint i, address sender) public view
-    // {
-    //     diff[i]=(_time1-_starttime1)/3600;
-    //         if(exptime[i]-diff[i] < 0)
-    //         {
-    //             penalty[sender]++;
-    //         }
-      
-    // }
-
-    function callCheck() external returns(uint256[4] memory,string memory, int[4] memory, int)
+    function callCheck() external returns(uint256,string memory, int)
     {
         
         if(msg.sender== 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4)
         {
         task t= task(addressT[0]);
-        (status[0], time1[0], starttime1[0], expp, sender) = t.check();
-        // compare(time1[0],starttime1[0],0, sender);
+        (status[0], time1[0], starttime1[0], expp, send1) = t.check();
+        // compare(time1[0],starttime1[0],0, send1);
         
         diff[0]=(time1[0]-starttime1[0]);
 
     /******** time comparison *********/
       if(int(exptime[0])-int(diff[0]) < 0)
       {
-          penalty1[0]++;
+          penalty[send1]++;
 
       }
       /*****************************************/
       /******** price comparison *********/
          if(int(expp.softwaremod)-int(excp[0]) < 0)
       {
-          penalty1[0]++;
+          penalty[send1]++;
       } 
        if(int(expp.webdev)-int(excp[1]) < 0)
       {
-          penalty1[0]++;
+         penalty[send1]++;
       } 
        if(int(expp.integration)-int(excp[2]) < 0)
       {
-          penalty1[0]++;
+         penalty[send1]++;
       } 
         if(int(expp.resources)-int(excp[3]) < 0)
       {
-          penalty1[0]++;
+         penalty[send1]++;
       } 
       /**************************************************/
 
       
 
         task1 t1= task1(addressT[1]);
-        (status[1], time1[1], starttime1[1], expp1, sender) = t1.check();
+        (status[1], time1[1], starttime1[1], expp1, send1) = t1.check();
         diff[1]=(time1[1]-starttime1[1]);
 
         if(int(exptime[1])-int(diff[1]) < 0)
             {
-                penalty1[1]++;
+               penalty[send1]++;
             }
 
         /******** price comparison *********/
          if(int(expp1.softwaremod)-int(excp1[0]) < 0)
       {
-          penalty1[1]++;
+          penalty[send1]++;
       } 
        if(int(expp1.webdev)-int(excp1[1]) < 0)
       {
-          penalty1[1]++;
+          penalty[send1]++;
       } 
        if(int(expp1.integration)-int(excp1[2]) < 0)
       {
-          penalty1[1]++;
+          penalty[send1]++;
       } 
         if(int(expp1.resources)-int(excp1[3]) < 0)
       {
-          penalty1[1]++;
+          penalty[send1]++;
       } 
       /**************************************************/
        
 
         task2 t2= task2(addressT[2]);
-        (status[2], time1[2], starttime1[2], expp2, sender) = t2.check();
+        (status[2], time1[2], starttime1[2], expp2, send1) = t2.check();
         diff[2]=(time1[2]-starttime1[2]);
         
         if(int(exptime[2])-int(diff[2]) < 0)
             {
-                penalty1[2]++;
+                penalty[send1]++;
             }
          /******** price comparison *********/
          if(int(expp2.softwaremod)-int(excp2[0]) < 0)
       {
-          penalty1[2]++;
+          penalty[send1]++;
       } 
        if(int(expp2.webdev)-int(excp2[1]) < 0)
       {
-          penalty1[2]++;
+          penalty[send1]++;
       } 
        if(int(expp2.integration)-int(excp2[2]) < 0)
       {
-          penalty1[2]++;
+          penalty[send1]++;
       } 
         if(int(expp2.resources)-int(excp2[3]) < 0)
       {
-          penalty1[2]++;
+          penalty[send1]++;
       } 
       /**************************************************/   
         
 
         task3 t3= task3(addressT[3]);
-        (status[3], time1[3], starttime1[3], expp3, sender) = t3.check();
+        (status[3], time1[3], starttime1[3], expp3, send1) = t3.check();
         diff[3]=(time1[3]-starttime1[3]);
        
         if(int(exptime[3])-int(diff[3])< 0)
             {
-                penalty1[3]++;
+                penalty[send1]++;
             }
 
          /******** price comparison *********/
          if(int(expp3.softwaremod)-int(excp3[0]) < 0)
       {
-          penalty1[3]++;
+          penalty[send1]++;
       } 
        if(int(expp3.webdev)-int(excp3[1]) < 0)
       {
-          penalty1[3]++;
+          penalty[send1]++;
       } 
        if(int(expp3.integration)-int(excp3[2]) < 0)
       {
-          penalty1[3]++;
+          penalty[send1]++;
       } 
         if(int(expp3.resources)-int(excp3[3]) < 0)
       {
-          penalty1[3]++;
+          penalty[send1]++;
       } 
       /**************************************************/  
-     
+     sprinttime= now;
          
 
         }
@@ -185,9 +171,9 @@ contract master{
         if(i==4)
         {
             // msg.sender.transfer(1 ether);
-            return (diff, "Proceed for payment",penalty1,1);
+            return (sprinttime, "Proceed for payment",1);
         }
-        return (diff, "Cancel",penalty1,-1);
+        return (sprinttime, "Cancel",-1);
     } 
     
 
--- Code/stake.sol
+++ Code/stake.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.6.0;
+pragma solidity 0.6.0;
 pragma experimental ABIEncoderV2;
 import 'master.sol';
 
@@ -6,7 +6,60 @@ import 'master.sol';
 contract stakeholder
 {
 
+    struct date1
+    {
+        uint days1;
+        uint month1;
+        uint year1;
+    }
+
    address addressM;
+
+    function countleapyears(date1 memory _d2) public returns(uint)
+    {
+        uint years2= _d2.year1;
+        // uint month2= _d2.month1;
+       if( _d2.month1 <=2)
+       {
+           years2--;
+       }
+
+       return (years2/4) - (years2/100) + (years2/400);
+    }
+    
+    uint exptime2;
+    function timestamp1(date1 memory _d1) public returns(uint)
+    {   
+        date1 memory _maind;
+        _maind.days1= 1;
+        _maind.month1=1;
+        _maind.year1=1970;
+
+        uint8[12] memory monthdays = [31,28,31,30,31,30,31,31,30,31,30,31];
+        uint256 n1= _maind.year1 * 365 + _maind.days1;
+
+        for (uint i = 0; i < _maind.month1 - 1; i++)
+        {
+		n1= n1+ monthdays[i];
+        }
+
+        n1= n1+ countleapyears(_maind);
+
+
+        uint256 n2= _d1.year1 * 365 + _d1.days1;
+
+        for (uint i = 0; i < _d1.month1 - 1; i++)
+        {
+		   n2= n2+ monthdays[i];
+        }
+        n2= n2+ countleapyears(_d1);
+
+	exptime2= ((n2 - n1)*86400) - 19800;
+    return exptime2;
+
+
+    }
+
 
     
     function addMasterAdd(address _addressM) external {
@@ -15,7 +68,7 @@ contract stakeholder
 
    
 
-   uint256[4] diff2;
+   uint256 time2;
    string status2;
    int[4] penalty2;
 
@@ -31,21 +84,35 @@ contract stakeholder
     }
     int flag;
 
-    function sendEtherAccount() public payable
+    function sendEtherAccount() public payable returns(uint)
     {
         address payable vendor = payable(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4);
        
         master m= master(addressM);
-        (diff2, status2, penalty2,flag) = m.callCheck();
+        (time2, status2,flag) = m.callCheck();
+
+        int extra= (int(time2)-int(exptime2))/86400;
+
        
         if(flag == 1)
             {
+                if(extra>0)
+                    {
+                        vendor.transfer(1 ether - 0.01 ether*uint(extra));
+                        }
+                else
+                {
                     vendor.transfer(1 ether);
+                }
+
+                   
             }
             else
             {
                 vendor.transfer(0 ether);
+                
             }
+            return time2;
     }
 
 
